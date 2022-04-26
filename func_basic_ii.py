ff39c21a55edad0c39d865f3800969ef3273bb9e
Lisa <lisa.hl.chen@gmail.com>
2022-04-26 15:12:05 -0700

    Adding solution format

--- func_basic_ii.py
+++ func_basic_ii.py
@@ -8,17 +8,23 @@ def countdown(cookie):
         cookie-=1
     return new_jar
 
+eat_cookie=countdown(5)
+print('Countdown',eat_cookie)
+
 # 2. Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
     # Example: print_and_return([1,2]) should print 1 and return 2
 
 def print_and_return(info):
     print(list[0])
     return list[1]
+list=[3,2,3]
+print('Print and Return',print_and_return(list))
 
 # 3. First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
     # Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
 def first_plus_length(list):
     return list[0]+len(list)
+print('First + Length',first_plus_length([3,2,5,3]))
 
 
 # 4. Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
@@ -36,6 +42,7 @@ def values_greater_than_second(list):
             values_great+=1
     print(len(new_list))
     return new_list
+print('Values Greater than Second',values_greater_than_second([1,1,5,3,3]))
 
 # 5. This Length, That Value - Write a function that accepts two integers as parameters: size and value. The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
     # Example: length_and_value(4,7) should return [7,7,7,7]
@@ -46,6 +53,8 @@ def length_value(size,value):
     for i in range(0,size):
         new_list.append(value)
     return new_list
+
+print('Length Value',length_value(5,2))
 
 
 
